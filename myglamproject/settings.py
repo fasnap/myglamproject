ee18f9496776f13981ee885e06ed7fb2dcecc44f
fasnap <fasnapkdy@gmail.com>
2021-11-02 21:21:42 +0530

    allowed host included

--- myglamproject/settings.py
+++ myglamproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True,cast=bool)
 
-ALLOWED_HOSTS = ['samplemyglam.fasnap.ml','www.samplemyglam.fasnap.ml']
+ALLOWED_HOSTS = ['3.129.20.94','127.0.0.1','samplemyglam.fasnap.ml','www.samplemyglam.fasnap.ml']
 
 
 # Application definition
